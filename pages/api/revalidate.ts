5a2ce6e8b97b55bafddb538e87e7adbab7eb3c44
Nerebb <nguyenhaitan001@gmail.com>
2022-12-12 14:51:55 +0700

    [/api/revalidate] still not autoupdate when deploy

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -9,8 +9,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-   // Check for secret to confirm this is a valid request
-   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+  // Check for secret to confirm this is a valid request
+
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 'password= 0kqhQVA77eLefXWl'
 
 import { createClient } from '@supabase/supabase-js'
