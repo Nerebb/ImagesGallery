b8fec2e4baa9e997b69f8ac77ef3e3781022668b
Nerebb <nguyenhaitan001@gmail.com>
2022-12-12 13:45:27 +0700

    Minor fix

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     URL: "https://xzlqgrispmodapsargkl.supabase.co",
     SERVICE_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bHFncmlzcG1vZGFwc2FyZ2tsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MDY0NTYwOSwiZXhwIjoxOTg2MjIxNjA5fQ.FEioxX2It5Rg60XaUjrpdga9PipO6WQIOHQxMZTRyr4",
-    REVALIDATE_SECRET: supersecret
+    REVALIDATE_SECRET: 'supersecret'
   },
 
   images: {
